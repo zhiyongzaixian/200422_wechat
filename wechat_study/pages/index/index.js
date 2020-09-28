f431a283a968e77b69b7decc545d6e70a7a401ca
zhiyongzaixian <13852292775@163.com>
2020-09-28 14:54:34 +0800

    '0928'

---
+++ wechat_study/pages/index/index.js
@@ -0,0 +1,86 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: '初始化的测试数据111'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('onLoad()')
+
+    // this.setData({
+    //   msg: '修改之后的值222'
+    // })
+    // console.log(this.data.msg) // 
+    // 修改状态数据: this.setData()
+    // setTimeout(() => {
+    //   this.setData({
+    //     msg: '修改之后的值222'
+    //   })
+    //   console.log(this.data.msg) // 
+    // }, 2000)
+    
+  },
+
+  handleParent(){
+    console.log('parent')
+  },
+  handleChild(){
+    console.log('child')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
