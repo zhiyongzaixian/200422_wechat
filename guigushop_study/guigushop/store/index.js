fbe9872f9727942ce0d5b3d9047f01ffce81610e
zhiyongzaixian <13852292775@163.com>
2020-10-14 17:04:59 +0800

    '1014'

--- guigushop_study/guigushop/store/index.js
+++ guigushop_study/guigushop/store/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import home from './modules/home.js'
-
+import cart from './modules/cart.js'
 // 面试题： Vue.use都做了哪些事情
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	modules: {
-		home
+		home,
+		cart
 	}
 })
 
---
+++ guigushop_study/guigushop/test/test.js
@@ -0,0 +1,3 @@
+let arr = [1,2,3]
+console.log(arr.find(item => item === 1))
+console.log(arr.find(item => item === 4))
